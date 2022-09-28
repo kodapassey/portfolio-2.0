514f50f716df7647ba5c0dc309afbee3951252b9
kodapassey <kodapassey@charter.net>
2022-09-28 10:42:22 -0700

    react-router changes

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,6 +1,7 @@
 import '../css/portfolio.css'
 import { Container, Row, Col } from "react-bootstrap";
 import runBuddy from '../images/projects/Screenshot (2).png';
+import portfolio from '../links/portfolio';
 
 
 export const Portfolio = () => {
@@ -10,34 +11,19 @@ export const Portfolio = () => {
                 <h1>View my work</h1>
                 <h2>Portfolio</h2>
 
-                <Col xl={3} lg={4} md={6} sm={6} xs={12} className="project">
-                    <img src={runBuddy} alt=""></img>
-                    <h3>Run Buddy</h3>
-                    <p>asdlfashdfgalsdhjfgalsdfhg</p>
-                    <button className="github">GitHub</button>
-                    <button className="live-demo">Live Demo</button>
-                </Col>
-                <Col xl={3} lg={4} md={6} sm={6} xs={12} className="project">
-                    <img src={runBuddy} alt=""></img>
-                    <h3>Run Buddy</h3>
-                    <p>asdlfashdfgalsdhjfgalsdfhg</p>
-                    <button className="github">GitHub</button>
-                    <button className="live-demo">Live Demo</button>
-                </Col>
-                <Col xl={3} lg={4} md={6} sm={6} xs={12} className="project">
-                    <img src={runBuddy} alt=""></img>
-                    <h3>Run Buddy</h3>
-                    <p>asdlfashdfgalsdhjfgalsdfhg</p>
-                    <button className="github">GitHub</button>
-                    <button className="live-demo">Live Demo</button>
-                </Col>
-                <Col xl={3} lg={4} md={6} sm={6} xs={12} className="project">
-                    <img src={runBuddy} alt=""></img>
-                    <h3>Run Buddy</h3>
-                    <p>asdlfashdfgalsdhjfgalsdfhg</p>
-                    <button className="github">GitHub</button>
-                    <button className="live-demo">Live Demo</button>
-                </Col>
+                {portfolio.map((project) => (
+                    <Col xl={3} lg={4} md={6} sm={6} xs={12} className="project">
+                        <img src={project.img} alt=""></img>
+                        <h3>{project.name}</h3>
+                        <p>{project.desc}</p>
+                        <a target="_blank" href={project.github}>
+                            <button className="github">GitHub</button>
+                        </a>
+                        <a target="_blank" href={project.live}>
+                            <button className="live-demo">Live Demo</button>
+                        </a>
+                    </Col>
+                ))}
             </Row>
         </Container >
     )
---
+++ src/links/portfolio.js
@@ -0,0 +1,55 @@
+import runBuddy from '../images/projects/Screenshot (2).png';
+
+const portfolio = [
+    {
+        name: 'Run Buddy',
+        github: 'https://github.com/kodapassey/run-buddy',
+        live: 'https://kodapassey.github.io/run-buddy/',
+        desc: 'Run buddy is an app built for personal trainers',
+        img: runBuddy
+    },
+    {
+        name: 'Taskinator',
+        github: 'https://github.com/kodapassey/Taskinator',
+        live: 'https://kodapassey.github.io/Taskinator/',
+        desc: 'Taskinator is a todo list appliaction',
+        img: runBuddy
+    },
+    {
+        name: 'TaskMaster Pro',
+        github: 'https://github.com/kodapassey/taskmaster-pro',
+        live: 'https://kodapassey.github.io/taskmaster-pro/',
+        desc: 'Taskinator is a todo list appliaction',
+        img: runBuddy
+    },
+    {
+        name: 'Calendar App',
+        github: 'https://github.com/kodapassey/Calendar-Challenge',
+        live: 'https://kodapassey.github.io/Calendar-Challenge/',
+        desc: 'With this calendar app you can create tasks for different times of the day, as the hour of the day passes, the time turns red',
+        img: runBuddy
+    },
+    {
+        name: 'Git-it-done',
+        github: 'https://github.com/kodapassey/git-it-done',
+        live: 'https://kodapassey.github.io/git-it-done/',
+        desc: 'With this app, you can search for a github user, and see their repositores with open issues.',
+        img: runBuddy
+    },
+    {
+        name: 'Weather Dashboard',
+        github: 'https://github.com/kodapassey/weather-dashboard',
+        live: 'https://kodapassey.github.io/weather-dashboard/',
+        desc: 'With this weather dashboard app, you can search for a city and view the current and future weather for the city.',
+        img: runBuddy
+    },
+    {
+        name: 'Code Quiz',
+        github: 'https://github.com/kodapassey/Code-Quiz',
+        live: 'https://kodapassey.github.io/Code-Quiz/',
+        desc: 'Code quiz that tests your Javascript abilities!',
+        img: runBuddy
+    },
+]
+
+export default portfolio;
