4caea50fcf9c1bee935455e8c566012e1adebcef
kodapassey <kodapassey@charter.net>
2022-09-29 09:42:01 -0700

    added images to portfolio

--- src/links/portfolio.js
+++ src/links/portfolio.js
@@ -1,55 +1,77 @@
 import runBuddy from '../images/projects/Screenshot (2).png';
+import eatYourLuck from '../images/projects/Screenshot (4).png';
+import calendarApp from '../images/projects/Screenshot (6).png';
+import gitItDone from '../images/projects/Screenshot (7).png';
+import weatherDashboard from '../images/projects/Screenshot (8).png';
+import codeQuiz from '../images/projects/Screenshot (9).png';
+import taskinator from '../images/projects/Screenshot (14).png';
+import taskMasterPro from '../images/projects/Screenshot (13).png';
+import fitWit from '../images/projects/Screenshot (12).png';
 
 const portfolio = [
-    {
-        name: 'Run Buddy',
-        github: 'https://github.com/kodapassey/run-buddy',
-        live: 'https://kodapassey.github.io/run-buddy/',
-        desc: 'Run buddy is an app built for personal trainers',
-        img: runBuddy
-    },
     {
         name: 'Taskinator',
         github: 'https://github.com/kodapassey/Taskinator',
         live: 'https://kodapassey.github.io/Taskinator/',
         desc: 'Taskinator is a todo list appliaction',
-        img: runBuddy
+        img: taskinator
     },
     {
         name: 'TaskMaster Pro',
         github: 'https://github.com/kodapassey/taskmaster-pro',
         live: 'https://kodapassey.github.io/taskmaster-pro/',
-        desc: 'Taskinator is a todo list appliaction',
-        img: runBuddy
-    },
-    {
-        name: 'Calendar App',
-        github: 'https://github.com/kodapassey/Calendar-Challenge',
-        live: 'https://kodapassey.github.io/Calendar-Challenge/',
-        desc: 'With this calendar app you can create tasks for different times of the day, as the hour of the day passes, the time turns red',
-        img: runBuddy
+        desc: 'TaskMaster Pro is a more advanced todo list application',
+        img: taskMasterPro
     },
     {
         name: 'Git-it-done',
         github: 'https://github.com/kodapassey/git-it-done',
         live: 'https://kodapassey.github.io/git-it-done/',
         desc: 'With this app, you can search for a github user, and see their repositores with open issues.',
-        img: runBuddy
-    },
-    {
-        name: 'Weather Dashboard',
-        github: 'https://github.com/kodapassey/weather-dashboard',
-        live: 'https://kodapassey.github.io/weather-dashboard/',
-        desc: 'With this weather dashboard app, you can search for a city and view the current and future weather for the city.',
-        img: runBuddy
+        img: gitItDone
     },
     {
         name: 'Code Quiz',
         github: 'https://github.com/kodapassey/Code-Quiz',
         live: 'https://kodapassey.github.io/Code-Quiz/',
         desc: 'Code quiz that tests your Javascript abilities!',
-        img: runBuddy
+        img: codeQuiz
     },
+    {
+        name: 'Eat Your Luck',
+        github: 'https://github.com/lillielovatt/eat-your-luck',
+        live: 'https://lillielovatt.github.io/eat-your-luck/',
+        desc: 'Choose a random playing card and recieve a fun recipe!',
+        img: eatYourLuck
+    },
+    {
+        name: 'Fit-Wit',
+        github: 'https://github.com/Jamsniffer/fit-wit',
+        live: 'https://frozen-citadel-35341.herokuapp.com/',
+        desc: 'A fitness app where you can create an account, and pick your own workout!',
+        img: fitWit
+    },
+    {
+        name: 'Calendar App',
+        github: 'https://github.com/kodapassey/Calendar-Challenge',
+        live: 'https://kodapassey.github.io/Calendar-Challenge/',
+        desc: 'With this calendar app you can create tasks for different times of the day, as the hour of the day passes, the time turns red',
+        img: calendarApp
+    },
+    {
+        name: 'Weather Dashboard',
+        github: 'https://github.com/kodapassey/weather-dashboard',
+        live: 'https://kodapassey.github.io/weather-dashboard/',
+        desc: 'With this weather dashboard app, you can search for a city and view the current and future weather for the city.',
+        img: weatherDashboard
+    },
+    {
+        name: 'Run Buddy',
+        github: 'https://github.com/kodapassey/run-buddy',
+        live: 'https://kodapassey.github.io/run-buddy/',
+        desc: 'Run buddy is an app built for personal trainers',
+        img: runBuddy
+    }
 ]
 
 export default portfolio;
